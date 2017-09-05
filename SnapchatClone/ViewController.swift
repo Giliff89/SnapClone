8bd8ea1ff026c169b464686bb110548bed4e0d1d
Gina Iliff <giliff@me.com>
2017-09-05 15:15:04 -0700

    Set up Firebase using CocoaPods

--- SnapchatClone/ViewController.swift
+++ SnapchatClone/ViewController.swift
@@ -15,9 +15,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Hide keyboard when user touches outside text box
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // Hide keyboard when user presses return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        mealDescriptionText.resignFirstResponder()
+        return true
     }
 
 
